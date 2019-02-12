299fa75caf55e970111479ff0b90662de08c56ba
Jakub Filipowicz <jakubf@gmail.com>
2019-02-12 16:37:46 +0100

    update tests according to emdas build changes

--- tests/asmtest.sh
+++ tests/asmtest.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+EMAS=build/emas
+EMDAS=../emdas/build/emdas
 BASEDIR=../em400/tests
 TESTDIRS="alu args cycle int mem mod multix ops registers vendor"
 
 for tests in $TESTDIRS ; do
 	for file in `ls -1 $BASEDIR/functional/$tests/*.asm` ; do
 		echo $file
-		build/src/emas -Oraw -o /tmp/emas.bin -I $BASEDIR/include $file
-		../emdas/build/src/emdas -c mx16 -na -o /tmp/emas.asm /tmp/emas.bin
-		build/src/emas -Oraw -c mx16 -o /tmp/emas2.bin /tmp/emas.asm
+		$EMAS -Oraw -o /tmp/emas.bin -I $BASEDIR/include $file
+		$EMDAS -c mx16 -na -o /tmp/emas.asm /tmp/emas.bin
+		$EMAS -Oraw -c mx16 -o /tmp/emas2.bin /tmp/emas.asm
 		cmp /tmp/emas.bin /tmp/emas2.bin
 	done
 done
