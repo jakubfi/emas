42c67e7872f84d15d7b37bf0649730ef1616436f
Jakub Filipowicz <jakubf@gmail.com>
2019-06-05 19:15:17 +0200

    Travis: fix tests

--- tests/asmtest.sh
+++ tests/asmtest.sh
@@ -6,9 +6,14 @@ BASEDIR=../em400/tests
 TESTDIRS="alu args cycle int mem mod multix ops registers vendor"
 
 for tests in $TESTDIRS ; do
-	for file in `ls -1 $BASEDIR/functional/$tests/*.asm` ; do
+	files=$(ls -1 $BASEDIR/functional/$tests/*.asm 2>/dev/null)
+	if [ -z "$files" ] ; then
+		echo "Missing tests"
+		exit 1
+	fi
+	for file in $files ; do
 		echo $file
-		$EMAS -Oraw -o /tmp/emas.bin -I $BASEDIR/include $file
+		$EMAS -Iasminc -Oraw -o /tmp/emas.bin -I $BASEDIR/include $file
 		$EMDAS -c mx16 -na -o /tmp/emas.asm /tmp/emas.bin
 		$EMAS -Oraw -c mx16 -o /tmp/emas2.bin /tmp/emas.asm
 		cmp /tmp/emas.bin /tmp/emas2.bin || exit 1
