4894f038a361798924d44318e08bd1d1b421e2ff
Jakub Filipowicz <jakubf@gmail.com>
2021-12-01 19:46:58 +0100

    update test runner script (fixes #3)

--- tests/asmtest.sh
+++ tests/asmtest.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-EMAS=build/emas
+if [ -z "$1" ] ; then
+	echo "Missing em400 tests directory"
+	echo "Usage: $0 <tests_directory>"
+	exit 1
+fi
+
+CURD=$(readlink -f .)
+EMAS=$(find $CURD -name emas -type f -executable)
+if [ -z "$EMAS" ] ; then
+	EMAS=$(find $CURD/.. -name emas -type f -executable)
+fi
+if [ -z "$EMAS" ] ; then
+	echo "emas binary not found in current or upper directory"
+	exit 2
+fi
+
 EMDAS=emdas
-BASEDIR=../em400/tests
-TESTDIRS="alu args cycle int mem mod multix ops registers vendor"
+BASEDIR=$1
+TESTDIRS="addr alu args cycle int mem mod multix ops registers vendor"
 
 for tests in $TESTDIRS ; do
 	files=$(ls -1 $BASEDIR/functional/$tests/*.asm 2>/dev/null)
@@ -12,10 +27,10 @@ for tests in $TESTDIRS ; do
 		exit 1
 	fi
 	for file in $files ; do
-		echo $file
+		test_name=$(basename $file)
 		$EMAS -Iasminc -Oraw -o /tmp/emas.bin -I $BASEDIR/include $file
 		$EMDAS -c mx16 -na -o /tmp/emas.asm /tmp/emas.bin
 		$EMAS -Oraw -c mx16 -o /tmp/emas2.bin /tmp/emas.asm
-		cmp /tmp/emas.bin /tmp/emas2.bin || exit 1
+		cmp /tmp/emas.bin /tmp/emas2.bin &>/dev/null || echo "$test_name: output differs"
 	done
 done
